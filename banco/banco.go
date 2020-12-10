013fef719fa5554141cf6e9937ea111eff36e670
leandrorochaadm <leandrorochaadm@gmail.com>
2020-12-10 09:24:31 -0400

    iniciando servidor

---
+++ banco/banco.go
@@ -0,0 +1,26 @@
+package banco
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Conectar() (*sql.DB, error) {
+	stringConexao := "root:123456Asdf@/cursogo?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		return nil, erro
+	}
+	defer db.Close()
+
+	// testando conexao com banco
+	if erro = db.Ping(); erro != nil {
+		return nil, erro
+	}
+
+	fmt.Println("Conexão banco dados está aberta")
+	return db, nil
+}
---
+++ crud-simples.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	stringConexao := "root:123456Asdf@/cursogo?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	// testando conexao com banco
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conexão banco dados está aberta")
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+
+	//fmt.Println(linhas)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", func(w http.ResponseWriter, r *http.Request) {
+
+	}).Methods(http.MethodPost)
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
